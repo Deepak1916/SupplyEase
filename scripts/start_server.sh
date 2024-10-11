5c2a78f3f01b735dc3d636d01f5d7d9501a641ad
Deepak <deepakv@Deepaks-MacBook-Air.local>
2024-10-11 21:36:06 +0100

    final commit

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/devops
-export FLASK_APP=app.py
-export FLASK_ENV=development
-flask run --host=0.0.0.0 --port=5000
+# export FLASK_APP=app.py
+# export FLASK_ENV=development
+/usr/bin/python3 -m flask run --host=0.0.0.0 --port=5000
 #gunicorn -w 3 -b 0.0.0.0:5000 app:app --daemon
