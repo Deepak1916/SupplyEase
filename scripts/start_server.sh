58ac4f80058fbf16196c06983e2c56b3dce538f5
Deepak <deepakv@Deepaks-MacBook-Air.local>
2024-10-14 13:43:00 +0100

    2nd chsnges

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,5 @@ cd /home/ec2-user/devops
 # export FLASK_APP=app.py
 # export FLASK_ENV=development
 #/usr/bin/python3 -m flask run --host=0.0.0.0 --port=5000
-/usr/bin/python3 -m gunicorn -b 0.0.0.0:5000 app:app --daemon
+/usr/bin/python3 -m nohup gunicorn -b 0.0.0.0:5000 app:app --daemon
+
