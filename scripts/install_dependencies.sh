efe9448704c35051b2fa754736612d747e73be34
Deepak <deepakv@Deepaks-MacBook-Air.local>
2024-10-19 00:03:43 +0100

    install bcrypt

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 cd /home/ec2-user/devops
 #sudo yum install python3 python3-pip -y
+
 #Install Nginx
 sudo yum install nginx
+
 # Install Gunicorn
 pip3 install gunicorn
 
+#Install bcrypt
+pip install bcrypt
+
 #Install Flask
 pip3 install Flask
+
 #pip3 install -r requirement.txt
 
 pip3 install boto3
