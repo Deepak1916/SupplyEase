ee2935c830435837095227f6f9cad9ccebbf6344
Deepak-nci <x23267101@student.ncirl.ie>
2024-11-18 20:59:01 +0000

    test4

--- supplier_db.py
+++ supplier_db.py
@@ -4,7 +4,7 @@ from boto3.dynamodb.conditions import Key # type: ignore
 #session = boto3.Session(profile_name='dpk-terraform-profile')
 
 # Define the endpoint URL (optional, for custom/local endpoints)
-dynamodb = session.resource(
+dynamodb = boto3.resource(
     'dynamodb',
     endpoint_url='https://dynamodb.us-east-1.amazonaws.com',
     region_name='us-east-1',
