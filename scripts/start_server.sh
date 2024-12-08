243d827033707574b814c49d37cc60341e866d1a
Deepak-nci <x23267101@student.ncirl.ie>
2024-12-08 20:37:22 +0000

    test2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,4 +4,4 @@
 cd /home/ec2-user/devops
 
 # Running application using Gunicorn in background using Daemon mode.
-/usr/bin/python3 -m  gunicorn -b 3.237.53.141:5000 app:app --daemon
+/usr/bin/python3 -m  gunicorn -b 127.0.0.1:5000 app:app --daemon
